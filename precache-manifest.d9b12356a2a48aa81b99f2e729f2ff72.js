67e8030bb366248a3ce99e102d5b024b83797ff5
RofixWork <wourkout123@gmail.com>
2020-10-03 15:52:24 +0100

    Updates

---
+++ precache-manifest.d9b12356a2a48aa81b99f2e729f2ff72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6e064a44f9923b28d2ef5b150fbfeca",
+    "url": "/messenger/index.html"
+  },
+  {
+    "revision": "a400a97ffdc7b644a9d9",
+    "url": "/messenger/static/css/main.f380eb28.chunk.css"
+  },
+  {
+    "revision": "131bcbf6f5b47c870ed7",
+    "url": "/messenger/static/js/2.ec0bf0d2.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/messenger/static/js/2.ec0bf0d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a400a97ffdc7b644a9d9",
+    "url": "/messenger/static/js/main.04a3f000.chunk.js"
+  },
+  {
+    "revision": "a435bb58fed3cf357430",
+    "url": "/messenger/static/js/runtime-main.768bdee4.js"
+  }
+]);
